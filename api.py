dfc43aca501aafde8e427dab71c14f79c751a978
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-06-06 21:15:32 +0200

    Removed pychromecast dependency

--- api.py
+++ api.py
@@ -1,24 +1,13 @@
 from Flask import Flask, request, jsonify
 import db
-import routing
-import cast
 
 api = Flask(__name__)
-
-##give a beacon, return a speaker
-#@api.route('/beacons/<BeaconID>/speaker', methods = ['GET'])
-#def speaker(BeaconID):
-    
-#return a list of all connected chromecasts as a JSON object
-@api.route('/chromecasts', methods = ['GET'])
-def chromecasts():
-    return jsonify(cast.discover())
 
 #route audio to given location, return whether the request succeeded or failed
 @api.route('/location', methods = ['POST'])
 def set_location():
     if request.headers['Content-Type'] == 'application/json':
-        cast.set_output(request.json['SpeakerID'])
+        #set_output(request.json['SpeakerID'])
 
         response = jsonify({'message': "POST Successful"})
 
