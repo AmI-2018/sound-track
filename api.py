df75345ecfdf71db01b7fe22deeeb6762355bc25
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-06-07 13:48:47 +0200

    Simple location-getting

--- api.py
+++ api.py
@@ -1,13 +1,19 @@
 from Flask import Flask, request, jsonify
+from tempfile import TemporaryFile
 import db
 
 api = Flask(__name__)
+
+
+current_location = TemporaryFile()
+
 
 #route audio to given location, return whether the request succeeded or failed
 @api.route('/location', methods = ['POST'])
 def set_location():
     if request.headers['Content-Type'] == 'application/json':
-        #set_output(request.json['SpeakerID'])
+        
+        current_location = request.json['BeaconID']
 
         response = jsonify({'message': "POST Successful"})
 
@@ -16,3 +22,9 @@ def set_location():
 
     return response
 
+#the client devices will check to determine whether they should be active
+#returns current location
+@api.route('/location', methods = ['GET'])
+def get_location():
+
+    return jsonify({'BeaconID': current_location})
