da78ad9adf291fff52e0c61715bd825c2c9a0380
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-06-14 18:58:27 +0200

    Added GET method for /locations and fixed JSON output bugs

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from Flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from tempfile import TemporaryFile
 import db
 
@@ -25,24 +25,46 @@ def set_location():
 #the client devices will check to determine whether they should be active
 #returns current location
 @api.route('/current_location', methods = ['GET'])
-def get_location():
+def get_current_location():
+    
+    try:
+        return jsonify({'BeaconID': current_location})
 
-    return jsonify({'BeaconID': current_location})
+    except:
+        return jsonify({'message': "no current_location set"})
 
 #adds location given BeaconID, SpeakerID and IP Address of client Pi
 @api.route('/locations', methods = ['POST'])
 def add_location():
-    if request.headers['Content-Type'] == 'aplication/json':
+    if request.headers['Content-Type'] == 'application/json':
 
         try:
             db.add_location(request.json['BeaconID'], request.json['SpeakerID'], request.json['IPAddr'])
+
         except:
 
-            response = jsonify({'message': "db.add_location() error"}]
+            response = jsonify({'message': "db.add_location() error"})
 
-        response = jsonify({'message': "POST Successful"}]
+        response = jsonify({'message': "POST Successful"})
 
     else:
-        response = jsonify({'message': "Invalid Request"}]
+        response = jsonify({'message': "Invalid Request"})
 
     return response
+
+
+
+#returns all locations BeaconID, SpeakerID and IPAddress of client Pi
+@api.route('/locations', methods = ['GET'])
+def get_all_locations():
+
+    locations = db.get_all_locations()
+
+    if not locations:
+        return jsonify({'message': "no locations found"})
+    
+    return jsonify({'locations': db.get_all_locations()})
+
+if __name__ == '__main__':
+    api.run()
+
