8f6e872e76e2b644c82000f572e222e822285b74
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-06-26 17:47:50 +0200

    Fixed bug where new stream opened every 2 seconds....

--- poll.py
+++ poll.py
@@ -1,7 +1,7 @@
 import subprocess
 import requests
 import time
-from config import SERVER
+from config import SERVER, MY_ID
 
 session = requests.Session()
 
@@ -9,21 +9,32 @@ session.headers.update({'Content-type': 'application/json'})
 
 url = SERVER + "/current_location"
 
+last_location = None
+
 while True:
 
-    if session.get(url)['BeaconID'] = MY_ID:
+    new_location = session.get(url)['beacon_id']
+
+    if new_location == MY_ID and last_location != MY_ID:
         command = "arecord -D hw:0,0 -f S16_LE -c2|nc {0} 3334 &;nc {0} 3333|aplay".format(SERVER)
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         
         #this is for debugging, there shouldn't be output in normal circumstances
         output, error = process.communicate()
 
-    else:
+        last_location = MY_ID
+
+    elif new_location != MY_ID:
         command = "pkill -f 'arecord' && pkill -f 'nc' && pkill -f 'aplay'"
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
        
          #this is for debugging, there shouldn't be output in normal circumstances
         output, error = process.communicate()
+        
+        last_location = new_location
 
-
+    #This clause could be removed but just for verbosity I'll leave it in for now
+    else:
+        last_location = new_location
+    
     time.sleep(2) 
