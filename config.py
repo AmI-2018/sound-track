b9252fdf0c784dd8b02fd7a68407a3355493e0ae
Garrett Smith <ggasmithh@users.noreply.github.com>
2018-06-26 13:40:55 +0200

    Added start/stop playback functionality. Removed TemporaryFile dep.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 #GLOBAL CONFIGS
 SERVER = '192.168.0.255'
 
+MY_ID = 'set_me_to_the_estimote_BeaconID_string'
 
--- poll.py
+++ poll.py
@@ -1,3 +1,4 @@
+import subprocess
 import requests
 import time
 from config import SERVER
@@ -9,9 +10,20 @@ session.headers.update({'Content-type': 'application/json'})
 url = SERVER + "/current_location"
 
 while True:
-    with open(current_location, 'w') as f:
-        f.seek(0)
-        f.write(session.get(url))
-        f.truncate()
+
+    if session.get(url)['BeaconID'] = MY_ID:
+        command = "arecord -D hw:0,0 -f S16_LE -c2|nc {0} 3334 &;nc {0} 3333|aplay".format(SERVER)
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        
+        #this is for debugging, there shouldn't be output in normal circumstances
+        output, error = process.communicate()
+
+    else:
+        command = "pkill -f 'arecord' && pkill -f 'nc' && pkill -f 'aplay'"
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+       
+         #this is for debugging, there shouldn't be output in normal circumstances
+        output, error = process.communicate()
+
 
     time.sleep(2) 
